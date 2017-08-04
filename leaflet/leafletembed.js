3cd6551678b734ca7ace00221787eb988ae0475c
Ángel <anglxd@gmail.com>
2017-08-04 17:34:00 +0200

    v.2.0
    Añadido el mapa y el layer de temperaturas.
    Añadidas también las temperaturas con JSon obtenido de OpenWeatherMap.

--- leaflet/leafletembed.js
+++ leaflet/leafletembed.js
@@ -4,21 +4,54 @@ var plotlist;
 var plotlayers=[];
 
 function initmap() {
-	// set up the map
-	map = new L.Map('map');
 
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 12, attribution: osmAttrib});
-  var temp = L.tileLayer('http://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=d22d9a6a3ff2aa523d5917bbccc89211', {
-      maxZoom: 18,
-      attribution: '&copy; <a href="http://owm.io">VANE</a>',
-      id: 'temp'
-  });
+	var cityIDs = "2512989,2514097,2520493,2514216,3124967,2514301,2516479";
 
-	// start the map in South-East England
-	map.setView(new L.LatLng(39.3694, 2.6502),9);
-  map.addLayer(osm);
-	map.addLayer(temp);
+	var mymap = L.map('map').setView([39.3694, 2.8502], 9);
+	//light_nolabels
+	//dark_nolabels
+	//light_all
+	//dark_all
+	//base-eco
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+		maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
+	}).addTo(mymap);
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png', {
+		maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
+	}).addTo(mymap);
+	var temp = L.tileLayer('http://{s}.tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=d22d9a6a3ff2aa523d5917bbccc89211&lang=es', {
+		maxZoom: 18,
+	  attribution: '&copy; <a href="https://openweathermap.org">OpenWeatherMap</a>',
+	  id: 'temp'
+	}).addTo(mymap);
+
+	$.getJSON('http://openweathermap.org/data/2.5/group?id='+cityIDs+'&units=metric&lang=es&appid=b1b15e88fa797225412429c1c50c122a1', function(data) {
+		ponerMarkers(data);
+	});
+
+	function ponerMarkers(json){
+		$.each(json.list, function(j, item){
+			//console.log(item);
+			var name = item.name;
+			var lat = item.coord.lat;
+			var lon = item.coord.lon;
+			var temperatura = item.main.temp;
+
+			var circulo = L.circle([lat,lon], {
+				color: 'black',
+				fillcolor: 'black',
+				fillOpacity: 1,
+				radius: 500
+			});
+
+			//circulo.bindPopup("Temperatura: " + temperatura);
+			var string = "<div class='row text-center'><span class = 'temp'>"+temperatura+"</div>";
+			circulo.bindTooltip(string, {
+				permanent: true,
+				direction: 'right',
+				offset: new L.Point(-3,-15)
+			});
+			circulo.addTo(mymap);
+		});
+	}
 }
